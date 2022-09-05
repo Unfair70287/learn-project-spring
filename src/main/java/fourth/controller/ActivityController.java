b96ce361f409190c4002425670bb0b10c0b88d2e
YUHSI <s0905364741@gmail.com>
2022-09-05 12:28:13 +0800

    活動20220905

--- src/main/java/fourth/controller/ActivityController.java
+++ src/main/java/fourth/controller/ActivityController.java
@@ -1,6 +1,9 @@
 package fourth.controller;
 
+import java.io.File;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import fourth.bean.ActivityBean;
 import fourth.service.ActivityService;
@@ -19,26 +24,11 @@ public class ActivityController {
 	@Autowired
 	private ActivityService activityService;
 
-	
-
 	// Activity get
 	@GetMapping("/Activity")
-	public String uerSelect(@RequestParam(required=false) String select, Model m) {
-		List<ActivityBean> activities = null;
-		if (select!= null ) {
-			String[] yearAndMonth = select.split("-");
-			if (yearAndMonth.length == 2) {
-				String year = yearAndMonth[0];
-				String month = yearAndMonth[1].replace("0", "");
-				String where = "where (DATEPART(yy, start_time) = " + year + " AND DATEPART(mm, start_time) = " + month
-						+ ")";
-				activities = activityService.selectActivity(where);
-			}
-		}
-		if (activities == null) {
-			activities = activityService.selectAllActivity();
-		}
-		m.addAttribute("selectAllActivity", activities);
+	public String uerSelect(@RequestParam(required = false) String select, Model m) {
+		List<ActivityBean> selectActivityBeans = selectActivityBeans(select);
+		m.addAttribute("selectAllActivity", selectActivityBeans);
 
 		return "Activity_User";
 	}
@@ -54,7 +44,7 @@ public class ActivityController {
 		return "Activity_OP";
 	}
 
-	//Activity_OP post
+	// Activity_OP post
 	@PostMapping("/Activity_OP")
 	public String activityOP(@RequestParam(required = false) String request, @ModelAttribute ActivityBean activityBean,
 			Model m) {
@@ -78,22 +68,11 @@ public class ActivityController {
 
 	}
 
-	//Activity_OP get
+	// Activity_OP get
 	@GetMapping("/Activity_OP")
-	public String opSelect(@RequestParam(value = "select") String select, Model m) {
-		List<ActivityBean> activities = null;
-		String[] yearAndMonth = select.split("-");
-		if (yearAndMonth.length == 2) {
-			String year = yearAndMonth[0];
-			String month = yearAndMonth[1].replace("0", "");
-			String where = "where (DATEPART(yy, start_time) = " + year + " AND DATEPART(mm, start_time) = " + month
-					+ ")";
-			activities = activityService.selectActivity(where);
-		}
-		if (activities == null) {
-			activities = activityService.selectAllActivity();
-		}
-		m.addAttribute("selectAllActivity", activities);
+	public String opSelect(@RequestParam(required = false) String select, Model m) {
+		List<ActivityBean> selectActivityBeans = selectActivityBeans(select);
+		m.addAttribute("selectAllActivity", selectActivityBeans);
 		return "Activity_OP";
 	}
 
@@ -102,10 +81,11 @@ public class ActivityController {
 	public String insertActivities() {
 		return "ActivityInster";
 	}
-	
-	//Activity_OP/update post
+
+	// Activity_OP/update post
 	@PostMapping("/Activity_OP_update")
-	public String updateActivities(@RequestParam(required = false) String request,@ModelAttribute ActivityBean activityBean, Model m) {
+	public String updateActivities(@RequestParam(required = false) String request,
+			@ModelAttribute ActivityBean activityBean, Model m) {
 		if (request != null) {
 			if (request.equals("修改")) {
 				System.out.println(activityBean.toString());
@@ -113,12 +93,39 @@ public class ActivityController {
 				return "ActivityUpdate";
 			} else if (request.equals("刪除")) {
 				activityService.deleteActivities(activityBean);
-				
+				return "redirect:/Activity_OP";
+
 			}
 		}
-		activityOP(null, activityBean, m);
-		return "Activity_OP";
+		return "redirect:/Activity_OP";
+	}
+	
+//	@PostMapping()
+//	@ResponseBody
+//	public String name(@RequestParam("myFiles") MultipartFile multipartFile) {
+//		String filename = multipartFile.getOriginalFilename();
+//		File file = new File(saveFlieDir,filename);
+//		
+//		
+//	}
+	
+	
+	private List<ActivityBean> selectActivityBeans(String select) {
+		List<ActivityBean> activities = null;
+		if (select != null) {
+			String[] yearAndMonth = select.split("-");
+			if (yearAndMonth.length == 2) {
+				String year = yearAndMonth[0];
+				String month = yearAndMonth[1].replace("0", "");
+				String where = "where (DATEPART(yy, start_time) = " + year + " AND DATEPART(mm, start_time) = " + month
+						+ ")";
+				activities = activityService.selectActivity(where);
+			}
+		}
+		if (activities == null) {
+			activities = activityService.selectAllActivity();
+		}
+		return activities;
 	}
 
-	
 }
